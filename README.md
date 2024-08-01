fe1b0f1dedc336f59c0fed7b47b218aa99921a46
Satyam Choudhary <115560964+ch-saty@users.noreply.github.com>
2024-08-01 16:13:35 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# 2d-Portfolio-Js
-my 2d-game portfolio 
+# Developer portfolio that's a 2D game
+
+Inspired by the many creative 3D portfolios out there and using my current learning skills on Game Development, I created one portfolio site for my own but it's 2D instead.
+
+# How to run
+
+Note: You need `Node.js` and `npm` installed on your machine.
+	If `Node.js` is not installed first install `Node.js` then proceed the steps below.
+`npm install` then `npm run dev`
+ 
---
+++ src/constants.js
@@ -0,0 +1,30 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Flutter and gameDev these days.
+    I've made a couple of games in that language. I also like C++ and Solidity. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/ch_saty" target="_blank" style="color:#ff66cc;">Github</a>!`,
+
+  "cs-degree": `This is my CS degree. I graduated from MMMUT-24 Batch. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+
+  "sofa-table": `That's my sofa. I like to relax here and play Clash Of Clans. 
+  I also play make nostalgic games sitting here sometimes like Tetris, Snake, etc.`,
+
+  tv: `That's my TV. I've been watching gameDev's a lot recently like :<br>
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank"  style="color:#ff66cc;">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank" style="color:#ff66cc;">Theo</a>,
+   <a href="https://www.youtube.com/@PirateSoftware" target="_blank" style="color:#ff66cc;">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" style="color:#ff66cc;" target="_blank">Melkey</a>!`,
+
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/ch-saty/APS/blob/main/resume_satyam.pdf" target="_blank" style="color:#ff66cc;">Check it out?</a>
+  Contact me at satyamchoudhary8183@gmail.com if you have any reletable job opportunities!`,
+
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by accessing the code provided in my Github <a href="https://github.com/ch-saty" target="_blank" style="color:#ff66cc;">here</a>.`,
+
+  library: `There are a lot of books on my shelves, most being Mathematics( I just love maths💜). There are also some novels, my favourite being "The art of letting go"
+  I probably only read one of them. Who else compulsively buys novels without ever finishing them?`,
+
+  exit: `If you want to exit Satyam's portfolio, just close the tab.`,
+};
